8b86301b57a41f6e872755120bb3131cd952d21a
FKatya <eafedot@gmail.com>
2024-10-21 22:38:47 +0300

    Добавлено получение изображений по тегу и без него

--- ApiAndCats.py
+++ ApiAndCats.py
@@ -20,7 +20,9 @@ def load_image(url):
         return None
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag=f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title('Картинки с котиками')
@@ -37,6 +39,10 @@ window = Tk()
 window.title('Котики')
 window.geometry('600x500')
 
+tag_entry = Entry()
+tag_entry.pack()
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
@@ -49,5 +55,6 @@ file_menu.add_command(label='Выход', command=exit)
 # update_button.pack()
 
 url = 'https://cataas.com/cat'
+
 
 window.mainloop()
