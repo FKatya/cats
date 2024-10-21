5d3d6b7e0d65cc45e3af8cec000d2b87f8e843bd
FKatya <eafedot@gmail.com>
2024-10-21 14:15:34 +0300

    Создана функция load_image - загрузка изображения

--- ApiAndCats.py
+++ ApiAndCats.py
@@ -3,6 +3,19 @@ from PIL import Image,ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Ошибка: {e}')
+        return None
+
+
 window = Tk()
 window.title('Котики')
 window.geometry('600x480')
