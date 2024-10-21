a80a23abb6c926b4e67bfa79e67c6cf511a17573
FKatya <eafedot@gmail.com>
2024-10-21 20:59:13 +0300

    Изменена функция set_image на open_new_window и добавлена возможность открывать изображения в новых окнах

--- ApiAndCats.py
+++ ApiAndCats.py
@@ -19,34 +19,35 @@ def load_image(url):
         print(f'Ошибка: {e}')
         return None
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Картинки с котиками')
+        new_window.geometry('600x480')
+        label = Label(new_window,image=img)
+        label.pack()
         label.image = img
 
 
-def exit:
+def exit():
     window.destroy()
 
 window = Tk()
 window.title('Котики')
-window.geometry('600x5')
-
-label = Label()
-label.pack()
+window.geometry('600x500')
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar,tearoff=0)
 menu_bar.add_cascade(label='Файл',menu=file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image())
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
 
 url = 'https://cataas.com/cat'
-set_image()
+
 window.mainloop()
